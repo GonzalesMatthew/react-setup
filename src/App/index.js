09efb6b7dc0a80ba99ccf7fa4093b75eea6ee8df
Matthew Gonzales <gonzalesmattg@gmail.com>
2021-04-21 17:18:40 -0700

    enable scss, move files, add eslint, add front end dependencies, update App/index.js

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <h2>INSIDE APP COMPONENT</h2>
+      <button className='btn btn-info'>I am a button</button>
+    </div>
+  );
+}
+
+export default App;
